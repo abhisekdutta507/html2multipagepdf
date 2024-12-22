e9b22b9ee0630629422f0bc6b32646c472764049
Abhisek Dutta <58113568+abhisekdutta507@users.noreply.github.com>
2024-12-22 22:33:22 +0530

    readme updated v2.0.8 (#12)
    * updated readme v2.0.8
    
    * readme updated v2.0.8

--- constants.d.ts
+++ constants.d.ts
@@ -1,30 +1,100 @@
-import {
-  PageMargin,
-  PageQuality,
-  OutputType,
-  PageGenerator,
-  PageOptions,
-} from "./types";
-
-export const RegularPageSelector: string;
-export const PageWithMaxPossibleWidthSelector: string;
-export const PageElementSelector: string;
-export const HiddenElementSelector: string;
-
-export const JSPDFOutputType: OutputType;
-
-export const Generator: PageGenerator;
-export const Quality: PageQuality;
-
-export const A4: PageOptions;
-export const Legal: PageOptions;
-export const Letter: PageOptions;
-
-export type {
-  PageOptions,
-};
-
 declare module "@abhisek507/html2multipagepdf/constants" {
+  type ImageCompression = "NONE" | "FAST" | "MEDIUM" | "SLOW";
+
+  type ImageFormat =
+    | "RGBA"
+    | "UNKNOWN"
+    | "PNG"
+    | "TIFF"
+    | "JPG"
+    | "JPEG"
+    | "JPEG2000"
+    | "GIF87a"
+    | "GIF89a"
+    | "WEBP"
+    | "BMP";
+
+  interface EncryptionOptions {
+    userPassword?: string;
+    ownerPassword?: string;
+    userPermissions?: ("print" | "modify" | "copy" | "annot-forms")[];
+  }
+
+  interface jsPDFOptions {
+    orientation?: "p" | "portrait" | "l" | "landscape";
+    unit?: "pt" | "px" | "in" | "mm" | "cm" | "ex" | "em" | "pc";
+    format?: string | number[];
+    compress?: boolean;
+    precision?: number;
+    filters?: string[];
+    userUnit?: number;
+    encryption?: EncryptionOptions;
+    putOnlyUsedFonts?: boolean;
+    hotfixes?: string[];
+    floatPrecision?: number | "smart";
+  }
+
+  interface RGBAData {
+    data: Uint8ClampedArray;
+    width: number;
+    height: number;
+  }
+
+  interface ImageOptions {
+    imageData:
+    | string
+    | HTMLImageElement
+    | HTMLCanvasElement
+    | Uint8Array
+    | RGBAData;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    alias?: string;
+    compression?: ImageCompression;
+    rotation?: number;
+    format?: ImageFormat;
+  }
+
+  export interface PageMargin {
+    narrow: number;
+    normal: number;
+  }
+
+  export interface PageQuality {
+    100: number;
+    90: number;
+    80: number;
+    70: number;
+    60: number;
+    50: number;
+    40: number;
+    30: number;
+    20: number;
+    10: number;
+  }
+
+  export interface PageOptions extends jsPDFOptions {
+    orientation: "p" | "portrait" | "l" | "landscape";
+    unit: "pt" | "px" | "in" | "mm" | "cm" | "ex" | "em" | "pc";
+    format: number[];
+    compress: boolean;
+    margin: PageMargin;
+  }
+
+  export interface OutputType {
+    arraybuffer: string;
+    blob: string;
+    bloburi: string;
+    datauristring: string;
+  }
+
+  export interface PageGenerator {
+    withMaxPossibleWidth: string;
+    withRegularWidth: string;
+  }
+
   export const RegularPageSelector: string;
   export const PageWithMaxPossibleWidthSelector: string;
   export const PageElementSelector: string;
--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,138 @@
-import { PageOptions, jsPDF } from "./types";
-
 declare module "@abhisek507/html2multipagepdf" {
-  export function generatePDF (
+  type ImageCompression = "NONE" | "FAST" | "MEDIUM" | "SLOW";
+
+  type ImageFormat =
+    | "RGBA"
+    | "UNKNOWN"
+    | "PNG"
+    | "TIFF"
+    | "JPG"
+    | "JPEG"
+    | "JPEG2000"
+    | "GIF87a"
+    | "GIF89a"
+    | "WEBP"
+    | "BMP";
+
+  interface EncryptionOptions {
+    userPassword?: string;
+    ownerPassword?: string;
+    userPermissions?: ("print" | "modify" | "copy" | "annot-forms")[];
+  }
+
+  interface jsPDFOptions {
+    orientation?: "p" | "portrait" | "l" | "landscape";
+    unit?: "pt" | "px" | "in" | "mm" | "cm" | "ex" | "em" | "pc";
+    format?: string | number[];
+    compress?: boolean;
+    precision?: number;
+    filters?: string[];
+    userUnit?: number;
+    encryption?: EncryptionOptions;
+    putOnlyUsedFonts?: boolean;
+    hotfixes?: string[];
+    floatPrecision?: number | "smart";
+  }
+
+  interface RGBAData {
+    data: Uint8ClampedArray;
+    width: number;
+    height: number;
+  }
+
+  interface ImageOptions {
+    imageData:
+    | string
+    | HTMLImageElement
+    | HTMLCanvasElement
+    | Uint8Array
+    | RGBAData;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    alias?: string;
+    compression?: ImageCompression;
+    rotation?: number;
+    format?: ImageFormat;
+  }
+
+  export class jsPDF {
+    constructor(options?: jsPDFOptions);
+    constructor(
+      orientation?: "p" | "portrait" | "l" | "landscape",
+      unit?: "pt" | "px" | "in" | "mm" | "cm" | "ex" | "em" | "pc",
+      format?: string | number[],
+      compressPdf?: boolean
+    );
+    // jsPDF plugin: addImage
+    addImage(
+      imageData:
+        | string
+        | HTMLImageElement
+        | HTMLCanvasElement
+        | Uint8Array
+        | RGBAData,
+      format: string,
+      x: number,
+      y: number,
+      w: number,
+      h: number,
+      alias?: string,
+      compression?: ImageCompression,
+      rotation?: number
+    ): jsPDF;
+    addImage(
+      imageData:
+        | string
+        | HTMLImageElement
+        | HTMLCanvasElement
+        | Uint8Array
+        | RGBAData,
+      x: number,
+      y: number,
+      w: number,
+      h: number,
+      alias?: string,
+      compression?: ImageCompression,
+      rotation?: number
+    ): jsPDF;
+    addImage(options: ImageOptions): jsPDF;
+    addPage(
+      format?: string | number[],
+      orientation?: "p" | "portrait" | "l" | "landscape"
+    ): jsPDF;
+    deletePage(targetPage: number): jsPDF;
+    output(): string;
+    output(type: "arraybuffer"): ArrayBuffer;
+    output(type: "blob"): Blob;
+    output(type: "bloburi" | "bloburl"): URL;
+    output(
+      type: "datauristring" | "dataurlstring",
+      options?: { filename?: string }
+    ): string;
+    getCurrentPageInfo(): PageInfo;
+    save(filename: string, options: { returnPromise: true }): Promise<void>;
+    save(filename?: string): jsPDF;
+  }
+
+  interface PageMargin {
+    narrow: number;
+    normal: number;
+  }
+
+  interface PageOptions extends jsPDFOptions {
+    orientation: "p" | "portrait" | "l" | "landscape";
+    unit: "pt" | "px" | "in" | "mm" | "cm" | "ex" | "em" | "pc";
+    format: number[];
+    compress: boolean;
+    margin: PageMargin;
+  }
+
+  export function generatePDF(
     pageSelectors: string[],
     pageOptions: PageOptions,
     elementSelector: string,
-    quality: number,
+    quality: number
   ): Promise<jsPDF>;
 }
-
-export function generatePDF (
-  pageSelectors: string[],
-  pageOptions: PageOptions,
-  elementSelector: string,
-  quality: number,
-): Promise<jsPDF>;
